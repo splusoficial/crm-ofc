3e1a5b5b7e261efb3f16a960297615a135bb5c2b
Raphael C A D <Raphael@MacBook.local>
2025-08-01 20:24:54 -0300

    pagina inicial sem sidebar corrigido

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '@/styles/globals.css'
 import Layout from '../Layout'
+import { useRouter } from 'next/router';
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  // Não usa Layout na home
+  if (router.pathname === '/') {
+    return <Component {...pageProps} />;
+  }
   return (
     <Layout>
       <Component {...pageProps} />
